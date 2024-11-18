c6b7d3ff0857b52eaf7abfcdb8b66e97d4e943a2
Pal-R4 <149600855+Pal-R4@users.noreply.github.com>
2024-11-18 23:13:07 +0530

    Time: 3 ms (25.34%), Space: 5.9 MB (100%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,26 @@
+bool isAlphaNumber(char c) {
+    if (c >= 'a' && c <= 'z') return true;
+    if (c >= 'A' && c <= 'Z') return true;
+    if (c >= '0' && c <= '9') return true;
+    return false;
+}
+bool isPalindrome(char* s) {
+    // Two pointers
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+
+    int left = 0, right = strlen(s) - 1;
+    while (left < right) {
+        while (left < right && !isAlphaNumber(s[left])) {
+            left++;
+        }
+        while (left < right && !isAlphaNumber(s[right])) {
+            right--;
+        }
+        if (tolower(s[left++]) != tolower(s[right--])) {
+            return false;
+        }
+    }
+    return true;
+}
