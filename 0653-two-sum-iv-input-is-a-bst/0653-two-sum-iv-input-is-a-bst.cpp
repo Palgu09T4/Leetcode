0ca4abf0ca6e374a73f5be77ec8ed9fa9fe1f63d
Pal-R4 <149600855+Pal-R4@users.noreply.github.com>
2024-12-01 18:03:38 +0530

    Time: 5 ms (35.72%), Space: 38.7 MB (27.37%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,54 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- class BSTIterator {
-public:
- stack<TreeNode*>mystack;
- bool reverse=true;
-void pushall(TreeNode* root)
-{
-    while(root!=NULL)
-    {
-        mystack.push(root);
-        if(!reverse)
-        root=root->left;
-        else
-        root=root->right;
-    }
-}
-    BSTIterator(TreeNode* root,bool isreverse) {
-        reverse =isreverse;
-        pushall(root);
-    }
-    
-    int next() {
-       TreeNode* temp=mystack.top();
-       mystack.pop();
-       if(!reverse)
-       pushall(temp->right);
-       else
-       pushall(temp->left);
-       return temp->val;
-    }
-    
-    bool hasNext() {
-        return !mystack.empty();
-    }
-};
 class Solution {
 public:
+set<int>s;
     bool findTarget(TreeNode* root, int k) {
-        if(!root)false;
-        BSTIterator l(root,false);
-        BSTIterator r(root,true);
-        int i=l.next();
-        int j=r.next();
-        while(i<j)
-        {
-            if(i+j==k)return true;
-            else if(i+j<k)i=l.next();
-            else j=r.next();
-        }
-        return false;
+       
+       if(!root)return false;
+       if(s.find(k-root->val)!=s.end())return true;
+       else
+       {
+             s.insert(root->val);
+       }
+     return findTarget(root->left,k)||findTarget(root->right,k);
     }
 };
