90be90b92496d76d3180f8526a7553eb98726124
Palgu09T4 <149600855+Palgu09T4@users.noreply.github.com>
2025-01-18 23:54:31 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.25%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
- void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visit) {
-        visit[node] = true;
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (isConnected[node][i] && !visit[i]) {
-                dfs(i, isConnected, visit);
+ void dfs(int node, vector<vector<int>>&isConnected, vector<bool>&vis) {
+        // mark the more as visited
+        vis[node] = true; 
+        for(int i=0;i<isConnected.size();i++)
+        {
+            if(isConnected[node][i] && !vis[i]) {
+                dfs(i,isConnected, vis); 
             }
         }
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        int numberOfComponents = 0;
-        vector<bool> visit(n);
-
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                numberOfComponents++;
-                dfs(i, isConnected, visit);
+        int n=isConnected.size(); 
+       vector<bool>vis(n); 
+        int cnt = 0; 
+        for(int i = 0;i<n;i++) {
+            // if the node is not visited
+            if(!vis[i]) {
+                // counter to count the number of provinces 
+                cnt++;
+               dfs(i, isConnected, vis); 
             }
         }
-
-        return numberOfComponents;
+        return cnt; 
+        
+    
     }
 };
