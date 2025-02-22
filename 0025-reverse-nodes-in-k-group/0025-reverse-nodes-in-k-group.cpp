61ee87122209304a5775377b5c6c189f605156a0
Palgu09T4 <149600855+Palgu09T4@users.noreply.github.com>
2025-02-22 21:33:24 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.47%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,60 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- ListNode* kthnodefun(ListNode* temp,int k)
+class Solution {
+public:
+ListNode* reversell(ListNode* head)
+{
+  ListNode* temp=head;
+   ListNode* prev=NULL;
+   while(temp!=NULL)
+   {
+     ListNode* front=temp->next;
+     temp->next=prev;
+     prev=temp;
+     temp=front;
+   }
+   return prev;
+}
+ ListNode* getk(ListNode* head,int k)
  {
-    k-=1;
+    k--;
+    ListNode* temp=head;
     while(temp!=NULL && k>0)
     {
-        k--;
         temp=temp->next;
+        k--;
     }
     return temp;
  }
- ListNode* reverse(ListNode* head)
- {
-    ListNode* temp=head;
-    ListNode* prev=NULL;
-    while(temp!=NULL)
-    {
-        ListNode* front=temp->next;
-        temp->next=prev;
-        prev=temp;
-        temp=temp=front;
-    }
-    return prev;
- }
-class Solution {
-public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* temp=head;
-        ListNode* prevlast=NULL;
+         ListNode* prevnode=NULL;
         while(temp!=NULL)
         {
-        ListNode* kthnode=kthnodefun(temp,k);
+        ListNode* kthnode=getk(temp,k);
         if(kthnode==NULL)
         {
-            if(prevlast)
+            if(prevnode)
             {
-                prevlast->next=temp;
-                break;
+                prevnode->next=temp;
             }
+            break;
         }
         ListNode* nextnode=kthnode->next;
         kthnode->next=NULL;
-        reverse(temp);
+
+        reversell(temp);
         if(temp==head)
         {
             head=kthnode;
-
         }
         else
         {
-            prevlast->next=kthnode;
+            prevnode->next=kthnode;
         }
-        prevlast=temp;
+        prevnode=temp;
         temp=nextnode;
         }
         return head;
+
     }
 };
